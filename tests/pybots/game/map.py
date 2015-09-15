e75e0467b51cc68255fc42854ffabb7b3f180147
Josef Kolář <thejoeejoee@gmail.com>
2015-09-15 22:07:31 +0200

    base packages, game map class with unit tests

---
+++ tests/pybots/game/map.py
@@ -0,0 +1,19 @@
+import unittest
+
+from pybots.game.map import Map, OutOfMapError
+
+
+class TestMap(unittest.TestCase):
+    def test_getitem(self):
+        game_map = Map(1, 1)
+        with self.assertRaises(AssertionError):
+            game_map[0]
+        with self.assertRaises(AssertionError):
+            game_map[0, 0, 0]
+
+        with self.assertRaises(OutOfMapError):
+            game_map[0, 1]
+        with self.assertRaises(OutOfMapError):
+            game_map[1, 0]
+
+        self.assertIsNone(game_map[0, 0], 'empty map')
