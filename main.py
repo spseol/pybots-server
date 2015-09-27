7aabfdcb7757a9a15724f856122ee7c2773f1a1e
Marek Nožka <nozka@spseol.cz>
2015-09-27 17:07:06 +0200

    easy run and with(out) debug

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from flask import Flask
+#!/usr/bin/env python
 
+import sys
+from flask import Flask
 from pybots.urls import urls
+
+PORT=44822
 
 
 app = Flask(__name__)
@@ -8,4 +12,7 @@ app = Flask(__name__)
 if __name__ == '__main__':
     for rule, view in urls:
         app.add_url_rule(rule, view_func=view)
-    app.run(debug=True)
+    if len(sys.argv) > 1 and sys.argv[1].upper() == 'DEBUG':
+        app.run(host='127.0.0.1', port=PORT,  debug=True)
+    else:
+        app.run(host='0.0.0.0', port=PORT,  debug=False)
