56823ba5b28114fade4c952bad81e40147254d95
Josef Kolář <thejoeejoee@gmail.com>
2015-10-11 22:03:25 +0200

    added bot_id casting to int

--- pybots/views/index_view.py
+++ pybots/views/index_view.py
@@ -9,9 +9,9 @@ class IndexView(MethodView):
     def get(self, *args, **kwargs):
         headers = sorted(request.headers.items())
         # headers.append(time())
-        bot_id = hash(
+        bot_id = abs(hash(
             tuple(headers)
-        )
+        ))
 
         game_controller.get(bot_id)
         return jsonify(id=bot_id, bot_id=bot_id)
