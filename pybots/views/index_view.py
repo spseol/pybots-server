a81aef5bdfb1855ede1af4c6b709c3416f8c7bf5
Josef Kolář <thejoeejoee@gmail.com>
2015-10-14 12:23:04 +0200

    to info view added fields, to id generating added time()

--- pybots/views/index_view.py
+++ pybots/views/index_view.py
@@ -1,6 +1,7 @@
 from flask.json import jsonify
 from flask.views import MethodView
 from flask import request
+from datetime import time
 
 from pybots.game.game_controller import game_controller
 
@@ -8,7 +9,7 @@ from pybots.game.game_controller import game_controller
 class IndexView(MethodView):
     def get(self, *args, **kwargs):
         headers = sorted(request.headers.items())
-        # headers.append(time())
+        headers.append(time())
         bot_id = abs(hash(
             tuple(headers)
         ))
--- pybots/views/info_view.py
+++ pybots/views/info_view.py
@@ -3,14 +3,16 @@ from flask.views import MethodView
 
 from pybots.game.actions import Action
 from pybots.game.orientations import Orientation
+from pybots.game.fields.fields import Fields
 
 
 class InfoView(MethodView):
     def get(self, *args, **kwargs):
         def enum_info_dict(enum):
-            return {member.value: member.name for member in enum}
+            return {str(member.name): str(member.value) for member in enum}
 
         return jsonify(
             action=enum_info_dict(Action),
-            orientation=enum_info_dict(Orientation)
+            orientation=enum_info_dict(Orientation),
+            fields=enum_info_dict(Fields)
         ), 200
