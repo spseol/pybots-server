ae6b1d3d72e5d91d76b1c3f5e128c47a11170f70
Josef Kolář <thejoeejoee@gmail.com>
2015-12-03 19:55:01 +0100

    rewritten game web detail to canvas

--- pybots/web/game_detail_view.py
+++ pybots/web/game_detail_view.py
@@ -1,5 +1,6 @@
-from flask.helpers import url_for
+from datetime import datetime
 
+from flask.helpers import url_for
 from flask.templating import render_template
 from flask.views import MethodView
 from werkzeug.utils import redirect
@@ -8,18 +9,22 @@ from pybots.game.game_controller import game_controller
 from pybots.views.utils import args_to_kwargs
 
 
-class GameView(MethodView):
+class GameDetailView(MethodView):
     decorators = [args_to_kwargs]
+
+    TIMEOUT_DELETE = 60
 
     def get(self, bot_id=None, **kwargs):
         game = game_controller.games.get(bot_id)
         if not game:
             return 'Game not found.', 404
-        return render_template('detail.html', game=game)
+        return render_template('detail.html', game=game, bot_id=bot_id)
 
     def post(self, bot_id=None, **kwargs):
         game = game_controller.games.get(bot_id)
         if not game:
             return 'Game not found.', 404
+        if game.last_modified_at.timestamp() + self.TIMEOUT_DELETE > datetime.now().timestamp():
+            return 'Game cannot be deleted', 403
         game_controller.remove_game(game)
         return redirect(url_for('admin_index'))
--- pybots/web/index_view.py
+++ pybots/web/index_view.py
@@ -9,6 +9,7 @@ from pybots.configurations.configuration_provider import configuration_provider
 from pybots.game.game_controller import game_controller
 from pybots.views.utils import form_to_kwargs
 from pybots.web.forms import ConfigurationForm
+from pybots.web.game_detail_view import GameDetailView
 
 
 class IndexView(MethodView):
@@ -20,7 +21,8 @@ class IndexView(MethodView):
                                games=game_controller.sorted_games(),
                                now=datetime.now(),
                                form=conf_form,
-                               set=set)
+                               set=set,
+                               TIMEOUT_DELETE=GameDetailView.TIMEOUT_DELETE)
 
     def post(self, **kwargs):
         form = ConfigurationForm(data=kwargs)
@@ -32,4 +34,5 @@ class IndexView(MethodView):
                                games=game_controller.sorted_games(),
                                now=datetime.now(),
                                form=form,
-                               set=set)
+                               set=set,
+                               TIMEOUT_DELETE=GameDetailView.TIMEOUT_DELETE)
