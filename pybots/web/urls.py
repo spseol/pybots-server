c32177b62b9cf1d9d8ce1d0753feb5887586d376
Josef Kolář <thejoeejoee@gmail.com>
2015-12-03 19:55:01 +0100

    rewritten game web detail to canvas

--- pybots/web/urls.py
+++ pybots/web/urls.py
@@ -1,8 +1,8 @@
-from pybots.web.detail_view import GameView
+from pybots.web.game_detail_view import GameDetailView
 from pybots.web.index_view import IndexView
 
 urls = (
     (u'/admin', IndexView.as_view('admin_index')),
-    (u'/admin/<int:bot_id>', GameView.as_view('admin_game_detail')),
-    (u'/admin/<int:bot_id>', GameView.as_view('admin_game_delete')),
+    (u'/admin/<int:bot_id>', GameDetailView.as_view('admin_game_detail')),
+    (u'/admin/<int:bot_id>', GameDetailView.as_view('admin_game_delete')),
 )
