ca231c0add66c2ffb57fd98ce2369fc56fab0fa6
Josef Kolář <thejoeejoee@gmail.com>
2016-03-16 19:38:49 +0100

    tests - hardcoded urls rewritten into url_for

--- tests/pybots/views/action_view.py
+++ tests/pybots/views/action_view.py
@@ -16,7 +16,7 @@ class TestActionView(TestCase):
         with self.test_client as c:
             bot_id = loads(c.get(url_for('init_game')).data).get('bot_id')
 
-            r = c.post('/action', data=dict(bot_id=bot_id, action=Action.TURN_LEFT.value))
+            r = c.post(url_for('action'), data=dict(bot_id=bot_id, action=Action.TURN_LEFT.value))
             assert isinstance(r, Response)
             self.assertEqual(r.status_code, 200, 'Valid request to turn left.')
             self.assertEqual(r.content_type, 'application/json')
@@ -38,13 +38,13 @@ class TestActionView(TestCase):
         with self.test_client as c:
             bot_id = loads(c.get(url_for('init_game')).data).get('bot_id')
 
-            r = c.post('/action', data=dict(bot_id='', action=''))
+            r = c.post(url_for('action'), data=dict(bot_id='', action=''))
             self.assertEqual(r.status_code, 404, 'Request with empty action and bot_id.')
 
-            r = c.post('/action', data=dict(bot_id='foobar', action=Action.TURN_LEFT.value))
+            r = c.post(url_for('action'), data=dict(bot_id='foobar', action=Action.TURN_LEFT.value))
             self.assertEqual(r.status_code, 404, 'Request with invalid bot_id.')
 
-            r = c.post('/action', data=dict(bot_id=bot_id, action='foobar'))
+            r = c.post(url_for('action'), data=dict(bot_id=bot_id, action='foobar'))
             self.assertEqual(r.status_code, 404, 'Request with invalid action.')
 
     def test_find_wall(self):
@@ -52,7 +52,7 @@ class TestActionView(TestCase):
             bot_id = loads(c.get(url_for('init_game')).data).get('bot_id')
             found_wall = False
             for turn in range(DefaultConfiguration.map_height):
-                r = c.post('/action', data=dict(bot_id=bot_id, action=Action.STEP.value))
+                r = c.post(url_for('action'), data=dict(bot_id=bot_id, action=Action.STEP.value))
                 self.assertEqual(r.status_code, 200)
                 if loads(r.data).get('state') in (ResponseState.MOVEMENT_ERROR.state, ResponseState.GAME_WON.state):
                     found_wall = True
@@ -63,7 +63,7 @@ class TestActionView(TestCase):
 
     def test_two_bots_in_map(self):
         with self.test_client as c:
-            c.get('/')  # TODO: remove this get, it's hide dependency on odd request to app
+            c.get(url_for('init_game'))  # TODO: remove this get, it's hide dependency on odd request to app
             bot_id_1 = loads(c.get(url_for('init_game')).data).get('bot_id')
             bot_id_2 = loads(c.get(url_for('init_game')).data).get('bot_id')
             self.assertNotEqual(bot_id_1, bot_id_2, 'Another bots')
@@ -88,10 +88,10 @@ class TestActionView(TestCase):
             blocks=blocks
         )
         with self.test_client as c:
-            c.post('/reset')
+            c.post(url_for('reset'))
             c = self.set_conf_to_test_client(conf, c)
             bot_id = loads(c.get(url_for('init_game')).data).get('bot_id')
-            r = c.get('/game/{}'.format(bot_id))
+            r = c.get(url_for('game', bot_id=bot_id))
             data = loads(r.data)
 
             self.assertEqual(
