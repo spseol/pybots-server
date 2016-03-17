1947a0b844dd7bb305a2ef4c8fc36f7206fa765f
Josef Kolář <thejoeejoee@gmail.com>
2016-03-17 22:17:17 +0100

    added name property for bot field, added generating random names for bots in map factory [#13]

--- pybots/game/fields/bot_field.py
+++ pybots/game/fields/bot_field.py
@@ -8,19 +8,32 @@ from pybots.game.utils import get_next_orientation
 class BotField(Field):
     DEFAULT_ORIENTATION = Orientation.NORTH
 
-    def __init__(self, orientation=DEFAULT_ORIENTATION):
-        assert isinstance(orientation, Orientation)
+    def __init__(self,
+                 orientation=DEFAULT_ORIENTATION,
+                 name=''):
+        assert isinstance(orientation, Orientation), 'orientation is instance of Orientation'
+        assert isinstance(name, str), 'bot name of str type'
         self._orientation = orientation
+        self._name = name
 
     def export(self, *args, **kwargs):
         return {
             FIELD_KEY: FieldEnum.BOT,
-            'orientation': self.orientation
+            'orientation': self.orientation,
+            'name': self._name
         }
 
     @property
     def orientation(self):
         return self._orientation
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
 
     def rotate(self, action):
         assert isinstance(action, Action)
